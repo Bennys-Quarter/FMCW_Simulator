167d2418b13ec0f20ccce077a0dcb0a3a14461b7
Bennys-Quarter <benedikt.privat@hotmail.com>
2025-04-29 11:38:27 +0200

    add file handler class

--- main.py
+++ main.py
@@ -2,22 +2,13 @@ import yaml
 import re
 
 from models.fmcw_radar import FMCWRadar
+from file_handler import FileHandler
 
-loader = yaml.SafeLoader
-loader.add_implicit_resolver(
-    u'tag:yaml.org,2002:float',
-    re.compile(u'''^(?:
-     [-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+]?[0-9]+)?
-    |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
-    |\\.[0-9_]+(?:[eE][-+][0-9]+)?
-    |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*
-    |[-+]?\\.(?:inf|Inf|INF)
-    |\\.(?:nan|NaN|NAN))$''', re.X),
-    list(u'-+0123456789.'))
 
 if __name__ == '__main__':
-    with open("input/signal_parameters.yaml", "r") as file:
-        config_dict = yaml.load(file, Loader=loader)
+    # Step 1: Load input parameters
+    f_handler = FileHandler()
+    chirp_param = f_handler.read_input()
 
-    radar = FMCWRadar(config_dict)
+    radar = FMCWRadar(chirp_param)
     radar.plot_transmit_chirp()
